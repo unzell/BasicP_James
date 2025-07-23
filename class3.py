880eae6480892ec37b1650159ce590c082bf3cf9
Parames Saelim <paramet103@gmail.com>
2025-07-23 11:16:54 +0700

    class3's course

---
+++ class3.py
@@ -0,0 +1,48 @@
+# haverice = True
+# havespoon = False
+# havehand = True
+
+# if haverice:
+#     if havespoon:
+#         print("กินข้าว")
+#     elif havehand:
+#         print("กินข้าวเหนียว")
+
+# grade = "-D"
+# val = int(input("score : "))
+# if 100 >= val >= 0:
+#     if 59 >= val >= 50:
+#         grade  = "D"
+#     if 69 >= val >= 60:
+#         grade  = "C"
+#     if 79 >= val >= 70:
+#         grade  = "B"
+#     if 100 >= val >= 80:
+#         grade  = "A"
+# print("Grade : ",grade)
+
+# i = 0
+# while  i< 5:
+#     print(i + 1)
+#     i += 1 
+
+# while True: 
+#     print("enter 1 เพื่อบวกเลข | enter 2 เพื่อ quit")
+#     choice = int(input("input : "))
+    
+#     if choice == 1: 
+#         num = int(input("enter the amount of time for the operation : "))
+#         sum = 0
+#         for i in range(num):
+#             val = int(input("value : "))
+#             sum += val
+#         print(sum)
+
+            
+#     if choice == 2:
+#         print("quitting..")
+#         break
+
+# for i in range(2):
+#     for j in range(3):
+#         print("#", end='')
